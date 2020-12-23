ff5f039c545cf95e334410025becd4305d889ee6
Technik_Tueftler <technik.tueftler@gmail.com>
2020-12-23 20:17:26 +0100

    update config files - script to create db tables - ignore vs code structures

--- files/script.sql
+++ files/script.sql
@@ -2,7 +2,10 @@ CREATE TABLE IF NOT EXISTS users (
 	UserID text PRIMARY KEY,
 	UserName text,
 	MessagesSent integer DEFAULT 0,
+	CountLogins integer DEFAULT 1,
+	LastLogin text DEFAULT CURRENT_TIMESTAMP,
 	Coins integer DEFAULT 0,
 	CoinLock text DEFAULT CURRENT_TIMESTAMP,
-	Warnings integer DEFAULT 0
+	Warnings integer DEFAULT 0,
+	LostCounter integer DEFAULT 0
 );
